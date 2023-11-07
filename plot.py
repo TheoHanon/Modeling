83909226b94b1d7fa4c8c608581fc8d83abb8a03
TheoHanon <99546301+TheoHanon@users.noreply.github.com>
2023-11-07 22:43:01 +0100

    modif heatmap q1

--- plot.py
+++ plot.py
@@ -1,41 +1,80 @@
 from model import *
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
+from matplotlib.colors import LinearSegmentedColormap
 
 
+def create_smooth_colormap():
+    # Define colors as (R, G, B)
+    sandybrown = (244/255, 164/255, 96/255)
+    gold = (255/255, 215/255, 0/255)
+    forestgreen = (34/255, 139/255, 34/255)
 
+    # Create a colormap with smooth transitions
+    cdict = {
+        'red':   [(0.0, sandybrown[0], sandybrown[0]),
+                    (0.05, sandybrown[0], sandybrown[0]),
+                  (0.06, gold[0], gold[0]),
+                  (0.7, gold[0], gold[0]),
+                  (0.71, forestgreen[0], forestgreen[0]),
+                  (1.0, forestgreen[0], forestgreen[0])],  # Ensure it ends with x=1
+        'green': [(0.0, sandybrown[1], sandybrown[1]),
+                    (0.05, sandybrown[1], sandybrown[1]),
+                  (0.06, gold[1], gold[1]),
+                  (0.7, gold[1], gold[1]),
+                  (0.71, forestgreen[1], forestgreen[1]),
+                  (1.0, forestgreen[1], forestgreen[1])],  # Ensure it ends with x=1
+        'blue':  [(0.0, sandybrown[2], sandybrown[2]),
+                (0.05, sandybrown[2], sandybrown[2]),
+                  (0.06, gold[2], gold[2]),
+                  (0.7, gold[2], gold[2]),
+                  (0.71, forestgreen[2], forestgreen[2]),
+                  (1.0, forestgreen[2], forestgreen[2])]   # Ensure it ends with x=1
+    }
+    
+    return LinearSegmentedColormap('CustomMap', cdict)
 
+def apply_custom_colormap(T_results, colormap):
+    # Normalize T_results to be in the range 0-1
+    T_norm = T_results / T_results.max()
 
-def plot_q11(T_arr, R_arr, t_span):
+    # Use the colormap to get the corresponding color for each value
+    return colormap(T_norm)
 
-    R_arr = np.linspace(0, 5, 100)  # [mm/day]
-    T0 = np.array([0.05, 0.25, 0.50, 0.75, 1])
+def plot_q11(T_arr, t_span):
+    R_arr = np.linspace(0, 5, 100)  # Define the range of R values
+    T0 = np.array([0.05, 0.25, 0.50, 0.75, 1])  # Define the initial T values
 
-    for i in range(len(T0)):
+    # Create a custom colormap
+    custom_colormap = create_smooth_colormap()
 
-        T_init = T0[i]
-        T_results = T_arr[:, i, :]
+    for i, T_init in enumerate(T0):
+        T_results = T_arr[:, i, :]  # Get the results for the current T_init
 
         plt.figure(figsize=(8, 6))
-        
-        # Plot the heatmap using imshow with the custom colormap
         extent = [t_span[0], t_span[1], R_arr.min(), R_arr.max()]
-        im = plt.imshow(T_results.T, extent=extent, origin='lower', aspect='auto', cmap="viridis", vmax = 1, vmin = 0)
         
-        # Add contour lines if desired
-        levels = np.linspace(0, 1, num=100)  # Adjust number of levels
+        # Plot the heatmap using imshow with the smooth custom colormap
+        im = plt.imshow(T_results.T, extent=extent, origin='lower', aspect='auto', cmap=custom_colormap, vmax=1, vmin=0)
         
         # Add colorbar for reference
-        plt.colorbar(im)
-        
+        cbar = plt.colorbar(im)
+
+        legend_elements = [
+            Line2D([0], [0], marker='o', color='w', label='Arid (T < 0.05)', markersize=15, markerfacecolor='sandybrown', linestyle='None'),
+            Line2D([0], [0], marker='o', color='w', label='Savannah (0.05 ≤ T ≤ 0.7)', markersize=15, markerfacecolor='gold', linestyle='None'),
+            Line2D([0], [0], marker='o', color='w', label='Forest (T > 0.7)', markersize=15, markerfacecolor='forestgreen', linestyle='None'),
+        ]
+        plt.legend(handles=legend_elements, loc='upper right', shadow=True)        
         # Label the axes and the plot
         plt.xlabel('Time [year]')
         plt.ylabel('R [mm/day]')
-        plt.title(rf'$T_0 = {T_init}$', fontsize = 16)
+        plt.title(rf'$T_0={T_init}$', fontsize=25)
         
         # Show the plot
         plt.show()
-    return 
+# Rest of the code remains unchanged...
+
 
 
 def plot_q12(T_arr, R_arr):
@@ -70,7 +109,7 @@ def plot_q12(T_arr, R_arr):
     ax.set_xlabel('R')
     ax.set_ylabel('T0')
     ax.set_zlabel('T(600)')
-    ax.set_title(r"Equilibrium states $T(600)$ with respect to R and $T_0$", fontsize=16)
+    # ax.set_title(r"Equilibrium states $T(600)$ with respect to R and $T_0$", fontsize=16)
 
     # Adjust the aspect ratio
     # ax.set_box_aspect([np.ptp(i) for i in [R_mesh.flatten(), T0_mesh.flatten(), T600.flatten()]])
@@ -112,10 +151,10 @@ def plot_q2_3D(T_arr):
 
     # Grid lines and 3D effect
     ax.grid(True)  # Show grid lines for better orientation
-    ax.set_xlabel('R')
-    ax.set_ylabel('T0')
+    ax.set_xlabel(r'R_0')
+    ax.set_ylabel(r'T_0')
     ax.set_zlabel('T(600)')
-    ax.set_title(r"Equilibrium states of $T(600)$ with respect to $R_0$ and $T_0$", fontsize=16)
+    # ax.set_title(r"Equilibrium states $T(600)$ with respect to $R_0$ and $T_0$", fontsize=16)
 
     # Adjust the aspect ratio
     # ax.set_box_aspect([np.ptp(i) for i in [R_mesh.flatten(), T0_mesh.flatten(), T600.flatten()]])
@@ -130,7 +169,7 @@ def plot_q2_3D(T_arr):
 if __name__ == "__main__":
     T_arr, R_arr, t_span = run_model1()
 
-    plot_q11(T_arr, R_arr, t_span)
+    plot_q11(T_arr,t_span)
     plot_q12(T_arr, R_arr)
 
     T_arr = run_model2()
