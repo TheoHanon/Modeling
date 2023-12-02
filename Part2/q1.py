2f176176d6dee9fa12e39d7bfae4efece0424621
leaninite2002 <100617777+leaninite2002@users.noreply.github.com>
2023-12-02 10:57:46 +0100

    Update q1.py

--- Part2/q1.py
+++ Part2/q1.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 import sympy as sy
 from matplotlib.lines import Line2D
 
-R, T, z = sy.symbols('R T z')
-
 # Constants representing the parameters for the model
 
 r_m = 0.3     # Maximum growth rate of tree cover [1/year]
@@ -16,28 +14,15 @@ h_f = 0.6     # Half-saturation constant for external mortality factors
 p = 7         # Hill coefficient for external mortality response
 k = 0.9       # Carrying capacity of tree cover
 b = 2         # Baseline tree reproduction rate [mm/day]
-r_R = 1 
-    # Rainfall regeneration rate [1/year]
-
+r_R = 1       # Rainfall regeneration rate [1/year]
 
 
 r = lambda x: r_m * x / (h_R + x)
 
-dTdt = r(R) * T * (1 - T/k) - m_n * T * h_n/ (h_n + T) - m_f * T * h_f**p / (T**p + h_f**p)
-
-# J = lambda t, x : sy.diff(dTdt, T).subs(R, x).subs(T, t)
-
-F = lambda x: sy.Poly(sy.expand(r(x) * z*(1 - z/k)*(z+h_n) *(z**p + h_f**p) - m_n*z*h_n*(z**p + h_f**p) - m_f*z*(z + h_n)*h_f**p))
-
-# a = lambda x: F(x).all_coeffs()
-
-
-
+# Jacobian of the system
 J = lambda T, R: r(R)*(1 - 2*T/k) - m_n*h_n**2/(T + h_n)**2 - m_f*h_f**p * (T**p - p*T**p + h_f**p) / (T**p + h_f**p)**2
 
-
-
-
+# Polynomial whose roots give the equilibria 
 a = lambda x: [-r(x)/k, r(x)*(1-h_n/k), h_n*r(x)-m_n*h_n, 0, 0, 0, 0, -r(x)*h_f**p / k, h_f**p *(r(x) - h_n / k *r(x) - m_f), (r(x)*h_n*h_f**p - m_n*h_n*h_f**p-m_f*h_f**p*h_n),0]
 
 R_init = np.linspace(0, 5, 200)
@@ -47,18 +32,15 @@ for r_init in R_init:
     for t_eq in T_eq:
         if (np.isreal(t_eq) and t_eq.real >=0):
             if (J(t_eq, r_init) < 0):
-                plt.plot(r_init, t_eq.real, 'ro')
-            elif (J(t_eq, r_init) > 0):
-                plt.plot(r_init, t_eq.real, 'bo')
-            else:
                 plt.plot(r_init, t_eq.real, 'go')
+            elif (J(t_eq, r_init) > 0):
+                plt.plot(r_init, t_eq.real, 'ro')
+            else:
+                plt.plot(r_init, t_eq.real, 'bo')
                 
-plt.legend(handles = [Line2D([0], [0], marker='o', color='r', label='attracting node', markerfacecolor='r', markersize=10), Line2D([0],[0], marker='o', color='b', label='repelling node', markerfacecolor='b', markersize=10)])
-plt.xlabel(r'$\bar{R}$')
-plt.ylabel(r'$\bar{T}$')
+plt.legend(handles = [Line2D([0], [0], marker='o', color='g', label='attractive node', markerfacecolor='g', markersize=10), Line2D([0],[0], marker='o', color='r', label='repulsive node', markerfacecolor='r', markersize=10)])
+plt.xlabel(r'$\overline{R}$[mm/day]')
+plt.ylabel(r'$\overline{T}$ [%]')
+plt.title(r"Graph of the equilibria $(\overline{T},\overline{R})$")
+plt.savefig("Q13.pdf")
 plt.show()
-
-
-
-
-
