14982e75f2e11b9e74a261001995605b7c6ca897
leaninite2002 <leaninite@hotmail.com>
2023-12-02 12:10:45 +0100

    3 plots

--- Part2/eq.py
+++ Part2/eq.py
@@ -29,9 +29,9 @@ fixedpointdata_africa = mat_file['fixedpointdata_africa']
 fixedpointdata_aus = mat_file['fixedpointdata_aus']
 
 # longitudes of southamerica, africa and australia [lower boundary value, laster boundary value]
-lons_sa = [-90, -30] # South america coordinates (longitude range)
-lons_af = [-30, 60] # African coordinates (longitude range)
-lons_au = [60, 160] 
+lons_sa = [-90, -30]    # South america coordinates (longitude range)
+lons_af = [-30, 60]     # African coordinates (longitude range)
+lons_au = [60, 160]     # Australia coordinates (longitude range)
 
 def unpack(x):
     return x[0][0]
@@ -54,7 +54,6 @@ def compute_forest2100(fixedpointdata, Lons, worstcase = False):
                 index = get_rainfall_index(rainfall2100[i,j])
                 T_arr = vunpack(fixedpointdata[index][1:3])
                 T_arr = T_arr[np.logical_not(np.isnan(T_arr))]
-                T_init = forest1999[i,j]
                 if worstcase:
                     forest2100[i,j] = T_arr[T_arr.argmin()]
                 else:
@@ -65,27 +64,25 @@ def compute_forest2100(fixedpointdata, Lons, worstcase = False):
 
 
 forest2100_america_wc = compute_forest2100(fixedpointdata_southamerica, lons_sa, worstcase = True)
-forest2100_america_bs = compute_forest2100(fixedpointdata_southamerica, lons_sa, worstcase = False)
-
-
+forest2100_america_bc = compute_forest2100(fixedpointdata_southamerica, lons_sa, worstcase = False)
 forest2100_africa_wc = compute_forest2100(fixedpointdata_africa, lons_af, worstcase = True)
-forest2100_africa_bs = compute_forest2100(fixedpointdata_africa, lons_af, worstcase = False)
+forest2100_africa_bc = compute_forest2100(fixedpointdata_africa, lons_af, worstcase = False)
 forest2100_aus_wc = compute_forest2100(fixedpointdata_aus, lons_au, worstcase = True)
-forest2100_aus_bs = compute_forest2100(fixedpointdata_aus, lons_au, worstcase = False)
+forest2100_aus_bc = compute_forest2100(fixedpointdata_aus, lons_au, worstcase = False)
 
 forest2100_wc = [forest2100_america_wc, forest2100_africa_wc, forest2100_aus_wc]
-forest2100_bs = [forest2100_america_bs, forest2100_africa_bs, forest2100_aus_bs]
+forest2100_bc = [forest2100_america_bc, forest2100_africa_bc, forest2100_aus_bc]
 
 
+def plot_forest_cover(forest_wc,forest_bc):
 
-def plot_forest_cover(forest_covers, case):
+    fig = plt.figure(figsize=(10, 10))
 
-    fig = plt.figure(figsize=(15, 12))
-
-    gs = gridspec.GridSpec(3, 1, height_ratios=[1, 1, 0.05], hspace=0.05)
+    gs = gridspec.GridSpec(4, 1, height_ratios=[1, 1, 1,0.05])
 
     ax1 = fig.add_subplot(gs[0], projection=ccrs.PlateCarree())
-    # First subplot for the year 1999
+
+    # Forest cover for 1999
     ax1.set_extent([min(lons), max(lons), min(lats), max(lats)], crs=ccrs.PlateCarree())
     ax1.add_feature(cfeature.OCEAN, alpha = .7)
     cs1 = ax1.contourf(lons, lats, forest1999, cmap='Greens', vmin=0, vmax=80, levels=np.linspace(0, 80, 80))
@@ -93,17 +90,27 @@ def plot_forest_cover(forest_covers, case):
     ax1.coastlines(resolution='110m', color='black', linewidth=1)
     ax1.gridlines(draw_labels=True, linewidth=0.5, color='gray', alpha=0.5, linestyle='--')
 
-    # Second subplot for the year 2100
+    # Forest cover for 2100 - best case
     ax2 = fig.add_subplot(gs[1], projection=ccrs.PlateCarree())
     ax2.set_extent([min(lons), max(lons), min(lats), max(lats)], crs=ccrs.PlateCarree())
-    for forest in forest_covers:
+    for forest in forest_bc:
         cs2 = ax2.contourf(lons, lats, forest, cmap="Greens", vmin=0, vmax=80, levels=np.linspace(0, 80, 80))
     ax2.add_feature(cfeature.OCEAN, alpha = 0.7)
-    ax2.set_title(f'Predicted {case} Case Forest Cover in 2100', fontsize = 14)
+    ax2.set_title(f'Predicted Best Case Forest Cover in 2100', fontsize = 14)
     ax2.coastlines(resolution='110m', color='black', linewidth=1)
     ax2.gridlines(draw_labels=True, linewidth=0.5, color='gray', alpha=0.7, linestyle='--')
 
-    cax = fig.add_subplot(gs[2])
+    # Forest cover for 2100 - worst case
+    ax3 = fig.add_subplot(gs[2], projection=ccrs.PlateCarree())
+    ax3.set_extent([min(lons), max(lons), min(lats), max(lats)], crs=ccrs.PlateCarree())
+    for forest in forest_wc:
+        cs3 = ax3.contourf(lons, lats, forest, cmap="Greens", vmin=0, vmax=80, levels=np.linspace(0, 80, 80))
+    ax3.add_feature(cfeature.OCEAN, alpha = 0.7)
+    ax3.set_title(f'Predicted Worst Case Forest Cover in 2100', fontsize = 14)
+    ax3.coastlines(resolution='110m', color='black', linewidth=1)
+    ax3.gridlines(draw_labels=True, linewidth=0.5, color='gray', alpha=0.7, linestyle='--')
+
+    cax = fig.add_subplot(gs[3])
     cbar = plt.colorbar(cs2, cax=cax, orientation='horizontal', pad = 0.01)
     cbar.set_label('Forest Cover (%)')
     cbar_ticks = np.arange(0, 81, 10)  # Creates a list from 0 to 80 in steps of 10
@@ -112,13 +119,11 @@ def plot_forest_cover(forest_covers, case):
     # Optionally, set custom tick labels
     cbar_ticklabels = [f'{tick}%' for tick in cbar_ticks]  # List comprehension to add '%' to each tick
     cbar.set_ticklabels(cbar_ticklabels)  # Sets the text of the ticks
-    fig.subplots_adjust(top=0.95, bottom=0.35, left=0.05, right=0.95, hspace=0.02)
+    fig.subplots_adjust(top=0.95, bottom=0.1, left=0.05, right=0.95, hspace=0.2)
+    plt.savefig("PredictedTreeCover.pdf")
     plt.show()
-
     return 
+plot_forest_cover(forest2100_wc,forest2100_bc)
 
-
-plot_forest_cover(forest2100_wc, case = 'Worst')
-plot_forest_cover(forest2100_bs, case = 'Best')
 
 
