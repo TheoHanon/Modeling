1ec705cd47ce670c57a65da03e3a7fb125a5fa1b
leaninite2002 <100617777+leaninite2002@users.noreply.github.com>
2023-12-13 14:49:46 +0100

    Update phasePortrait.py

--- Part2/phasePortrait.py
+++ Part2/phasePortrait.py
@@ -1,26 +1,18 @@
 import numpy as np
-from matplotlib.widgets import Slider
-from scipy.integrate import solve_ivp
-import matplotlib.animation as animation
 import numpy as np
-import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
-import scipy.io
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-
 
 R = 2         # Amount of rainfall [mm/day]
-r_m = 0.3    # Maximum growth rate of tree cover [1/year]
+r_m = 0.3     # Maximum growth rate of tree cover [1/year]
 h_R = 0.5     # Half-saturation constant for rainfall effect on growth [mm/day]
 m_n = 0.15    # Natural mortality rate of trees [1/year]
 h_n = 0.1     # Half-saturation constant for natural mortality
-m_f = 0.11   # Mortality rate of trees due to external factors [1/year]
+m_f = 0.11    # Mortality rate of trees due to external factors [1/year]
 h_f = 0.6     # Half-saturation constant for external mortality factors
 p = 7         # Hill coefficient for external mortality response
 k = 0.9       # Carrying capacity of tree cover
 b = 2         # Baseline tree reproduction rate [mm/day]
-r_R = 1      # Rainfall regeneration rate [1/year]
+r_R = 1       # Rainfall regeneration rate [1/year]
 
 
 # Growth rate of tree cover as a function of rainfall
@@ -28,14 +20,15 @@ r = lambda x: r_m * x / (h_R + x)
 
 ### New parameters
 alpha1= 0.1
-r_H = .01  # Human growth rate [1/year]
-K = .8  # Carrying capacity of humans 
+r_H = .01   # Human growth rate [1/year]
+K = .8      # Carrying capacity of humans 
 pi1 = .1
 alpha2 = 0.01
 lam = .01
 lam0 = .1
 R_const = 2
-# Differential equation for tree cover growth
+
+# Differential equations
 def dTdt(y1, y3 , y4,K):
     return (r(R_const) * y1 * (1 - y1 / k) - 
             m_n * y1 * h_n / (y1 + h_n) -
@@ -45,10 +38,11 @@ def dHdt(y1, y3, y4,K):
     return r_H * y3*(1 - y3/K) + pi1 * alpha1* y1*y3
 
 def dPdt(y1, y3, y4, K):
-    return lam*y3 - lam0*y4
+    return lam*(y3-y4) - lam0*y4
 
 t = np.linspace(0, 200, 201)
 
+# Euler explicit simulation
 def computeVariables(t,T0,H0,P0,K):
     T = [T0]
     H = [H0]
@@ -66,82 +60,49 @@ def computeVariables(t,T0,H0,P0,K):
     return T, H, P
 
 t = np.linspace(0, 200, 201)
-
-# En pleine forêt --> K petit, T grand, H et P petits
-K=0.1
-T0=0.9
-H0=0.05
-P0=0.05
-
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-T, H, P = computeVariables(t,T0, H0, P0,K)
-line, = ax.plot(T, H, P)
-line2, = ax.plot(T[0], H[0], P[0], 'or', label='t = 0 [day]')
-line3, = ax.plot(T[-1], H[-1], P[-1], 'og', label='t = 200 [day]')
-
-ax.set_xlabel('Tree cover [%]')
-ax.set_ylabel('Population density [%]')
-ax.set_zlabel('Population demand [%]')
-ax.legend()
-fig.suptitle("Phase portrait of the model in the heart of the forest", fontsize=15)
-fig.subplots_adjust(left=0.25, bottom=0.25)
-ax.set_ylim(0, 100)  
-ax.set_xlim(0, 100) 
-ax.set_zlim(0, 100)   
-plt.show()
-
-# Loin de la forêt --> K grand, T petit, H et P grands
-K=0.8
-T0=0.15
-H0=0.5
-P0=0.3
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-T, H, P = computeVariables(t,T0, H0, P0,K)
-line, = ax.plot(T, H, P)
-line2, = ax.plot(T[0], H[0], P[0], 'or', label='t = 0 [day]')
-line3, = ax.plot(T[-1], H[-1], P[-1], 'og', label='t = 200 [day]')
-
-ax.set_xlabel('Tree cover [%]')
-ax.set_ylabel('Population density [%]')
-ax.set_zlabel('Population demand [%]')
-ax.legend()
-fig.suptitle("Phase portrait of the model far from the forest", fontsize=15)
-fig.subplots_adjust(left=0.25, bottom=0.25)
-ax.set_ylim(0, 100)  
-ax.set_xlim(0, 100) 
-ax.set_zlim(0, 100)   
-plt.show()
-
-
-# Pas trop loin de la forêt --> K moyen, T, H et P moyen
+# Near the forest --> K, T, H and P intermediate
 K=0.5
 T0=0.5
 H0=0.2
 P0=0.15
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
 T, H, P = computeVariables(t,T0, H0, P0,K)
-line, = ax.plot(T, H, P)
 line2, = ax.plot(T[0], H[0], P[0], 'or', label='t = 0 [day]')
 line3, = ax.plot(T[-1], H[-1], P[-1], 'og', label='t = 200 [day]')
+line, = ax.plot(T, H, P, label='Near the forest')
+
+# Far from the forest --> K big, T small, H and P big
+K=0.8
+T0=0.15
+H0=0.5
+P0=0.3
+
+T, H, P = computeVariables(t,T0, H0, P0,K)
+line, = ax.plot(T, H, P,label='Far from the forest')
+line2, = ax.plot(T[0], H[0], P[0], 'or')
+line3, = ax.plot(T[-1], H[-1], P[-1], 'og')
+
+# In the heart of the forest --> K small, T big, H and P small
+K=0.1
+T0=0.9
+H0=0.05
+P0=0.05
+
+T, H, P = computeVariables(t,T0, H0, P0,K)
+line, = ax.plot(T, H, P,label='In the heart of the forest')
+line2, = ax.plot(T[0], H[0], P[0], 'or')
+line3, = ax.plot(T[-1], H[-1], P[-1], 'og')
 
 ax.set_xlabel('Tree cover [%]')
 ax.set_ylabel('Population density [%]')
 ax.set_zlabel('Population demand [%]')
 ax.legend()
-fig.suptitle("Phase portrait of the model near the forest", fontsize=15)
+fig.suptitle("Phase portrait of the model", fontsize=15)
 fig.subplots_adjust(left=0.25, bottom=0.25)
 ax.set_ylim(0, 100)  
 ax.set_xlim(0, 100) 
 ax.set_zlim(0, 100)   
 plt.show()
-
-
-
